3aae6e0d863ca12b4faf0e3d70ae366155796e33
alwil <alexandra.wilczynska@gmail.com>
2022-10-20 16:20:58 +0200

    gitautosync update 8

--- data-carpentry/scripts/script.R
+++ data-carpentry/scripts/script.R
@@ -2,3 +2,8 @@
 library(tidyverse)
 
 # this is a comment
+
+# Downloading data
+
+# download.file("https://ndownloader.figshare.com/files/11492171",
+#              "data/SAFI_clean.csv", mode = "wb")
