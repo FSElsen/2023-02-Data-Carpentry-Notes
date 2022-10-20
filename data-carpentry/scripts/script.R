01d9ca1e129f9a3cacf1b956d3f2cc605e9656c5
alwil <alexandra.wilczynska@gmail.com>
2022-10-20 16:28:24 +0200

    gitautosync update 10

--- data-carpentry/scripts/script.R
+++ data-carpentry/scripts/script.R
@@ -4,7 +4,13 @@ library(tidyverse)
 # this is a comment
 
 # Downloading data
-
 # download.file("https://ndownloader.figshare.com/files/11492171",
 #              "data/SAFI_clean.csv", mode = "wb")
 
+
+# R as calculator
+
+3 + 5
+12/7
+12*7
+3^5 # 3 to the power of 5
