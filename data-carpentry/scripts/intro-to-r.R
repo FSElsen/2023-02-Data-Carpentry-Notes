aa72925f275d244738b259b14b3896b29cfa3810
alwil <alexandra.wilczynska@gmail.com>
2022-10-24 10:43:56 +0200

    gitautosync update 6

--- data-carpentry/scripts/intro-to-r.R
+++ data-carpentry/scripts/intro-to-r.R
@@ -3,4 +3,8 @@ print("hello world")
 
 # install.packages("tidyverse") # uncomment if no tidyverse installed
 
+# Code to download data 
+download.file("https://ndownloader.figshare.com/files/11492171",
+              "data/SAFI_clean.csv", mode = "wb")
+
 
