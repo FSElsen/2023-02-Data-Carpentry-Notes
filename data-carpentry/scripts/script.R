585fade01baa2abceab80b7f14ad25580a7e3791
alwil <alexandra.wilczynska@gmail.com>
2022-10-25 10:08:19 +0200

    gitautosync update 13

--- data-carpentry/scripts/script.R
+++ data-carpentry/scripts/script.R
@@ -9,6 +9,12 @@ print("hello world")
 #download.file("https://ndownloader.figshare.com/files/11492171",
 #              "data/SAFI_clean.csv", mode = "wb")
 
+# R as a calculator
 
+3 + 5
+12/7
+12 * 7
+3 ^ 5 # 3 to the power of 5
 
+# Creating objects
 
